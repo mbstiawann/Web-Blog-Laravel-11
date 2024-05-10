7fc8b3de3ecd58ea14f5e53ea9a96709969c3a9d
mbstiawann <bags792stw@gmail.com>
2024-05-10 11:44:33 +0700

    set nav

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman Contact</title>
+
+</head>
+
+<body>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/blog">Blog</a></li>
+        <li><a href="/contact">Contact</a></li>
+    </ul>
+
+    <h1>Halamat Contact</h1>
+
+    <ul>
+        <li><a href="https://instagram.com/mbstiawan">Instagram</a></li>
+        <li><a href="https://github.com/mbstiawan">Github</a></li>
+        <li><a href="">085150021000</a></li>
+    </ul>
+</body>
+
+</html>
